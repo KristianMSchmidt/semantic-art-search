f6f425221480df7101f48c812db27a82f2aab9fc
Kristian Møller Schmidt <kmollerschmidt@gmail.com>
2025-02-02 04:15:34 +0000

    removed reload

--- djangoconfig/urls.py
+++ djangoconfig/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('artsearch.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
